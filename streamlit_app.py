de796a001dddfb396b2a7dcfbcd2f6f28bbcde43
shirinsummer <144369158+shirinsummer@users.noreply.github.com>
2023-09-07 13:56:03 -0400

    Update streamlit_app.py
    changed title
    changed header
    moved around emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit
 
-streamlit.title('Breakfast Favorites')
+streamlit.title('My Mom's New Healthy Diner')
 
-streamlit.header('🥣 Breakfast Menu')
-streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
